34d1f6e9af2b3b281a3b6ea51a699f8ad7bdf05c
hudamnhd <muhamadhuda519@gmail.com>
2023-05-16 11:14:27 +0700

    dwm flexipatch

---
+++ dwm-flexipatch/autostart.sh
@@ -0,0 +1,17 @@
+exec xrandr --output HDMI-1 --gamma 0.95:0.95:0.85 --brightness 0.8  --output DP-1 --gamma 0.95:0.95:0.85 --brightness 0.8 &
+xset r rate 250 60 &
+nitrogen --restore &
+lxpolkit    &
+exec xrandr --output DP-1 --auto --right-of HDMI-1 &
+pavucontrol &
+setxkbmap -layout "us" &              
+nm-applet &
+picom -b &
+while sleep 1
+do
+  
+  RAM=`free -m | grep "Mem" | awk '{ print $3 }'`
+  DATE=`date`
+    xsetroot -name "|RAM $RAM|$DATE |VOL $(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 2 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%"
+done &
+while type dwm >/dev/null; do dwm && continue || break; done
