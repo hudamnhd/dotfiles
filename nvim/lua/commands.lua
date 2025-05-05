c32ea1e72964b88e84ed544dedabd7d1e4eede77
hudamnhd <muhamadhuda519@gmail.com>
2025-05-06 02:28:43 +0700

    refactor: splite config file

---
+++ nvim/lua/commands.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = 'user_cmds',
+    desc = 'Highlight text after is copied',
+    callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 80})
+    end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = 'user_cmds',
+    pattern = {'qf', 'help', 'man'},
+    command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = 'user_cmds',
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,51 @@
+local bind = function(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+local nm, tm, cm, vm, nvm = "n", "t", "c", { "o", "x" }, { "n", "o", "x" }
+
+-- BASIC KEYMAP
+bind(tm, [[<c-\>]], [[<C-\><C-n>]])
+bind(tm, [[<a-x>]], [[<C-\><C-n>:bd!<Cr>]])
+bind(tm, [[<a-w>]], [[<C-\><C-n><c-w>w]])
+bind(tm, [[<a-r>]], [['<C-\><C-N>"'.nr2char(getchar()).'pi']], { expr = true })
+
+bind(cm, "<c-v>", [[<C-R>"]], { silent = false })
+bind(cm, "<a-v>", [[<C-R>+]], { silent = false })
+
+bind(cm, "<F1>", [[\(.*\)<Left><Left>]], { silent = false })
+bind(cm, "<F4>", 'getcmdtype() == ":" ? expand("%:p")  : ""', { silent = false, expr = true })
+
+bind(vm, ">", [[>gv]])
+bind(vm, "<", [[<gv]])
+
+bind(nm, "!", [[:<up><cr>]])
+bind(nm, "<a-w>", [[<c-w>w]])
+
+bind(nm, "<space>c", vim.cmd.close, { desc = "split" })
+bind(nm, "<space>s", vim.cmd.vsplit, { desc = "split" })
+bind(nm, "<space>w", vim.cmd.write, { desc = "Write" })
+
+bind(nm, "J", [['mz' . v:count1 . 'J`z']], { desc = "Join", expr = true })
+
+bind(nm, "cl", [[mzguiw`z]], { desc = "UPPERCASE to lowercase" })
+bind(nm, "ct", [[mzguiwgUl`z]], { desc = "Titlecase" })
+bind(nm, "cu", [[mzgUiw`z]], { desc = "lowercase to UPPERCASE" })
+
+bind(nvm, "0", ":", { silent = false })
+
+bind(nvm, "c", [["_c]])
+bind(nvm, "x", [["_x]])
+
+bind(nvm, "<space>y", [["+y]], { desc = "Yank to clipboard (primary)" })
+bind(nvm, "<space>p", [["+p]], { desc = "Paste after from clipboard (primary)" })
+
+bind(vm, "p", [['pgv"' . v:register . 'y']], { desc = "paste without replacing register", expr = true })
+
+bind(nvm, "<c-z>", [[%]])
+bind(nvm, "<c-l>", [[g_]])
+bind(nvm, "<c-h>", [[^]])
+
+
