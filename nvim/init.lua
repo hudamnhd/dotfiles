38fdbf8a2f00f2127ba0a4c7c308ac083f8d090a
hudamnhd <muhamadhuda519@gmail.com>
2025-05-06 02:07:31 +0700

    feat: added autocmd yank and cmd trailwhitespace

--- nvim/init.lua
+++ nvim/init.lua
@@ -85,6 +85,52 @@ bind(nvm, "<c-l>", [[g_]])
 bind(nvm, "<c-h>", [[^]])
 
 
+vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = 'user_cmds',
+  desc = 'Highlight text after is copied',
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 80})
+  end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = 'user_cmds',
+  pattern = {'qf', 'help', 'man'},
+  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+vim.api.nvim_create_user_command(
+  'TrailspaceTrim',
+  function()
+    local curpos = vim.api.nvim_win_get_cursor(0)
+
+    vim.cmd([[keeppatterns %s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, curpos)
+  end,
+  {desc = 'Delete extra whitespace'}
+)
+
+vim.api.nvim_create_user_command(
+  'SyntaxQuery',
+  function()
+    local f = vim.fn
+    local stack = f.synstack(f.line('.'), f.col('.'))
+
+    if stack[1] == nil then
+      print('No id found')
+      return
+    end
+
+    for _, id in pairs(stack) do
+      print(f.synIDattr(id, 'name'))
+    end
+  end,
+  {desc = 'Show highlight group'}
+)
+
+
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
