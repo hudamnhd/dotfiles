c040fb4470363bd82b6bc4c450a332fa250704da
hudamnhd <muhamadhuda519@gmail.com>
2025-05-07 11:36:05 +0700

    chore(config): update luarc, stylua and delete un used

--- nvim/plugin/05_substitute.lua
+++ nvim/plugin/05_substitute.lua
@@ -133,17 +133,6 @@ M.visual_charwise_selection = function()
 end
 
 
-local bind = function(mode, lhs, rhs, opts)
-  opts = opts or {}
-  opts.silent = opts.silent ~= false
-  vim.keymap.set(mode, lhs, rhs, opts)
-end
-
-local feedkeys = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, false, true), mode, false)
-end
-
-
 local function cgn(pattern)
   feedkeys("<esc>", "v")
   local cmd = vim.api.nvim_replace_termcodes('<CR>N"_cgn', true, false, true)
